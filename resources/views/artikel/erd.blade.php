1b9be592cd60135478f55ade0f14919aa8fabd0f
Elza Fadli Hadimulyo <elzafadli@gmail.com>
2020-07-05 19:33:39 +0700

    UP Ketinggalan Mebur

---
+++ resources/views/artikel/erd.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <img src="{{asset('images/erd.png') }}" alt="">
+    </div>
+  </div>
+@endsection
